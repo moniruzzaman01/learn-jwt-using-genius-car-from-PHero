bd5297d47fcf25bac1c7c2896060027acc965974
moniruzzaman01 <moniruzzamanshakib04@gmail.com>
2022-04-28 19:58:47 +0600

    initial commit

---
+++ server/index.js
@@ -0,0 +1,119 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pr56l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+function jwtVerify(req, res, next) {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.send({ message: "token not found!!!" });
+  }
+  jwt.verify(
+    token.split(" ")[1],
+    process.env.ACCESS_TOKEN_SECRET,
+    (err, decode) => {
+      if (err) {
+        return res.send({ message: "error found" });
+      }
+      // console.log("decoded", decode);
+      //to access from order
+      req.decode = decode;
+      //have to use this next function
+      next();
+    }
+  );
+}
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("geniusCar").collection("service");
+    const orderCollection = client.db("geniusCar").collection("orders");
+
+    //
+    app.get("/orders", jwtVerify, async (req, res) => {
+      const decodedEmail = req.decode.email;
+      console.log("decodedEmail:- ", decodedEmail);
+      const email = req.query.email;
+      if (decodedEmail === email) {
+        const cursor = orderCollection.find({ email });
+        const result = await cursor.toArray();
+        res.send(result);
+      } else {
+        res.send({ message: "emails doesnot match!!!" });
+      }
+    });
+
+    //JWT
+    app.post("/login", (req, res) => {
+      const user = req.body;
+      console.log("uesr", user);
+      const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+      res.send({ accessToken });
+    });
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    // POST
+    app.post("/service", async (req, res) => {
+      const newService = req.body;
+      const result = await serviceCollection.insertOne(newService);
+      res.send(result);
+    });
+
+    //
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+
+    // DELETE
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running Genius Server");
+});
+
+app.listen(port, () => {
+  console.log("Listening to port", port);
+});
